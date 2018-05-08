25a6047a6f0bd6b2643cc36a6d32a26e92b4a48d
Andrew Purcell <apurcell910@gmail.com>
2018-05-07 21:56:29 -0700

    Making all states prefabs their own js file, moving Player.js to prefabs folder

---
+++ game/js/states/Load.js
@@ -0,0 +1,10 @@
+var Load = function(game) {};
+Load.prototype = {
+	preload: function() {
+		console.log("Load: preload");
+	},
+	create: function() {
+		console.log("Load: create");
+		game.state.start('Play');
+	},
+}
