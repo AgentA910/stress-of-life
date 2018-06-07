f9c629e89c38bb87d81388faca9cf6c05dd0f1e4
nick olds <nolds7722@gmail.com>
2018-06-06 17:03:43 -0700

    bar and finish
    change bar and added text to finish (needs to be formated)

--- game/js/states/Finish.js
+++ game/js/states/Finish.js
@@ -5,10 +5,14 @@ Finish.prototype = {
 	},
 	create: function() {
 		console.log("Finish: create");
-		this.add.text(16, 16, "Game finished\nPress F to play again");
+		this.background = this.game.add.image(0, 0, 'backgroundMenu');
+		this.add.text(0, 400, "Press ENTER to play again");
+		this.add.text(0,50, "They say a little stress can lead to productivity,");
+		this.add.text(0,100, "but too much stress can only damage your health.");
+		this.add.text(0,200, "You have taken on too much stress.");
 	},
 	update: function() {
-		if (justPressed(Phaser.Keyboard.F)) {
+		if (justPressed(Phaser.Keyboard.ENTER)) {
 			game.state.start('Menu');
 		}
 	}
